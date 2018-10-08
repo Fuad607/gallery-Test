9d6ba55c0f1755a885fa8c66e259f1e5afd307d7
Fuad607 <35783045+Fuad607@users.noreply.github.com>
2018-10-08 13:37:42 +0200

    Update upload.blade.php

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <title>Laravel Multiple File Upload Example</title>
+    <title>Upload File</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
